81d39e80e98f33bbd83f65677d2f4f8efac25ea6
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2024-06-01 14:18:34 -1000

    updated some plots

--- Scripts/PPworking_Nyssa.R
+++ Scripts/PPworking_Nyssa.R
@@ -2,6 +2,8 @@
 ### Nyssa Silbiger ####
 ### 9/8/2023 #####
 
+### updated 5/31/24
+
 
 ## load libraries #####
 library(googlesheets4)
@@ -17,15 +19,108 @@ library(interactions)
 #Read google sheets data into R.
 
 ## Primary production and calcification data 
-PP<-read_sheet("https://docs.google.com/spreadsheets/d/1dl085D-DqLS5M1LfWZ60QpDHhI2_fPdhknwNtqxy6WM/edit?usp=sharing") %>%
+PP<-read_csv(here("Data","PP_Data.csv"))%>%
+  # read_sheet("https://docs.google.com/spreadsheets/d/1dl085D-DqLS5M1LfWZ60QpDHhI2_fPdhknwNtqxy6WM/edit?usp=sharing") %>%
   mutate_at(vars(daily_NPP:UPDN_No, daily_NEC:night_NEC_SE), .funs = as.numeric)%>%
   mutate(NEC_NEP = daily_NEC/daily_NPP, ## calculate NEP/NEC ratio
          NEC_R = night_NEC/daily_R)
 
 # Read in Benthic data 
 # Benthic Cover data
-BenthicCover <- read_sheet('https://docs.google.com/spreadsheets/d/1iA8rP_raCQ8NTPUGqAZ6LVeYza7eKqU4ep6yucpj1U0/edit?usp=sharing')%>%
+BenthicCover <- read_csv(here("Data","Backreef_Data.csv"))%>%
+  # read_sheet('https://docs.google.com/spreadsheets/d/1iA8rP_raCQ8NTPUGqAZ6LVeYza7eKqU4ep6yucpj1U0/edit?usp=sharing')%>%
   mutate_at(vars(coral:sub_squares), .funs = as.numeric)
+
+# Bob's transect data
+BenthicCover_Algae<-read_csv(here("Data","Backreef_Algae.csv")) %>%
+  filter(Habitat == "Backreef")
+
+
+
+Benthic_summary_Algae<-BenthicCover_Algae %>%
+  rename(name = Taxonomy_Substrate_Functional_Group)%>%
+  mutate(name = ifelse(name %in%c("Amansia rhodantha",         
+                    "Turbinaria ornata" ,        
+                    "Dictyota sp.",              
+                    "Halimeda sp.",              
+                    "Galaxaura sp.",             
+                    "Liagora ceranoides",        
+                    "Cyanophyta",                
+                    "Halimeda minima",           
+                    "Amphiroa fragilissima",     
+                    "Caulerpa serrulata",        
+                    "Corallimorpharia",          
+                    "Dictyota friabilis",        
+                    "Galaxaura rugosa",          
+                    "Cladophoropsis membranacea",
+                    "Galaxaura filamentosa",     
+                    "Halimeda discoidea",        
+                    "Peyssonnelia inamoena",     
+                    "Caulerpa racemosa",         
+                    "Valonia ventricosa",        
+                    "Actinotrichia fragilis",    
+                    "Dictyota bartayresiana",    
+                    "Microdictyon umbilicatum",  
+                    "Halimeda distorta",         
+                    "Halimeda incrassata",       
+                    "Halimeda macroloba",        
+                    "Dictyota implexa",          
+                    "Gelidiella acerosa",        
+                    "Dictyosphaeria cavernosa",  
+                    "Valonia aegagropila",       
+                    "Microdictyon okamurae",     
+                    "Halimeda opuntia",          
+                    "Dichotomaria obtusata",     
+                    "Chlorodesmis fastigiata",   
+                    "Phyllodictyon anastomosans",
+                    "Phormidium sp.",            
+                    "Cladophoropsis luxurians",  
+                    "Sargassum pacificum",       
+                    "Chnoospora implexa",        
+                    "Halimeda taenicola",        
+                    "Boodlea kaeneana",          
+                    "Padina boryana",            
+                    "Coelothrix irregularis",    
+                    "Gelidiella sp.",            
+                    "Hydroclathrus clathratus",  
+                    "Dictyota divaricata",       
+                    "Hypnea spinella",           
+                    "Dichotomaria marginata",    
+                    "Sporolithon sp.",           
+                    "Chaetomorpha antennina",    
+                    "Asparagopsis taxiformis"
+  ), "Fleshy Macroalgae",name))%>%
+  group_by(Year, Site, name)%>%
+  summarise(mean_cover = mean(Percent_Cover, na.rm = TRUE))
+
+LTER1_cover<-Benthic_summary_Algae %>%
+  filter(name %in% c("Coral","Crustose Corallines","Fleshy Macroalgae"),
+         Site == "LTER 1")%>%
+  ggplot(aes(x = Year, y = mean_cover, color = name))+
+  geom_point()+
+  geom_line()+
+  labs(x = "",
+       y = "Percent Cover",
+       color = "",
+       title = "LTER 1 only")+
+  theme_bw()
+
+All_cover<-Benthic_summary_Algae %>%
+  filter(name %in% c("Coral","Crustose Corallines","Fleshy Macroalgae"))%>%
+  group_by(Year, name)%>%
+  summarise(mean_all = mean(mean_cover, na.rm = TRUE),
+            se_all = sd(mean_cover, na.rm = TRUE)/sqrt(n()))%>%
+  ggplot(aes(x = Year, y = mean_all, color = name))+
+  geom_point()+
+  geom_errorbar(aes(ymin = mean_all-se_all, ymax = mean_all+se_all), width = 0.1)+
+  geom_line()+
+  labs(x = "",
+       y = "Percent Cover",
+       color = "",
+       title = "Mean across all 6 LTER sites")+
+  theme_bw()
+  
+LTER1_cover+ theme(legend.position = "none")|All_cover+ plot_layout(guides = "collect")
 
 ## Read in the deployment physics Data (light, temperature, and flow rate at time of collection) 
 
@@ -37,19 +132,43 @@ PP <- PP %>%
 
 ### look at relationship between NEP and NEC over time ###
 
-PP %>% 
+PP_long<-PP %>% 
   filter(Site == "LTER 1") %>%
-  ggplot()+
-  geom_point(aes(x = Year, y = daily_NEC), color = "grey")+
-  geom_point(aes(x = Year, y = night_NEC), color = "black")+
+  drop_na(daily_NEC)%>%
+  select(Month, Year, NEC = daily_NEC, SE = daily_NEC_SE, NEP = daily_GPP, SE_NEP = GPP_SE)%>%
+  mutate(Day_Night = "Day") %>%
+  bind_rows(PP %>% 
+              filter(Site == "LTER 1") %>%
+              drop_na(daily_NEC)%>%
+              select(Month, Year, NEC = night_NEC, SE = night_NEC_SE,NEP = daily_R, SE_NEP = R_SE)%>%
+              mutate(Day_Night = "Night")) 
+
+NEC_plot<-PP_long %>%  
+ggplot(aes(x = Year, y = NEC, color = Day_Night))+
+  geom_point()+
+  geom_errorbar(aes(ymin = NEC-SE, ymax = NEC+SE), width = 0.1)+
+  geom_smooth(method = "lm")+
   facet_wrap(~Month)
+
+NEP_plot<-PP_long %>%  
+  ggplot(aes(x = Year, y = NEP, color = Day_Night))+
+  geom_point()+
+  geom_errorbar(aes(ymin = NEP-SE_NEP, ymax = NEP+SE_NEP), width = 0.1)+
+  geom_smooth(method = "lm")+
+  facet_wrap(~Month)
+
+NEC_plot/NEP_plot
 
 PP %>% 
   filter(Site == "LTER 1") %>%
+  drop_na(daily_NEC)%>%
   ggplot(aes(x = Year, y = daily_NEC-night_NEC))+
   geom_point()+
+  labs(y = "Net Calcification")+
   geom_smooth(method = "lm")+
-  facet_wrap(~Month, scales = "free")
+  facet_wrap(~Month)
+
+
 
 ## NEC/NEC for day
 PP %>% 
@@ -84,13 +203,25 @@ Benthic_summary %>%
   facet_wrap(~Site)
 
 Benthic_summary %>%
+#  filter(Site == "LTER 1") %>%
+  select(!logratio)%>%
+  pivot_longer(coral:ctb) %>%
+  filter(name != "ctb")%>%
+  ggplot(aes(x = Year, y = value, color = name))+
+  geom_point()+
+  geom_line()+
+  labs(y = "Percent Cover")+
+  facet_wrap(~Site)
+  
+
+Benthic_summary %>%
 full_join(PP)  %>%
   filter(Site == "LTER 1")%>%
   drop_na(Month)%>%
 #  filter(Year != 2015) %>%
   ggplot(aes(x = logratio, y = daily_GPP))+
   geom_point()+
-  geom_smooth(method = "lm")+
+  geom_smooth(method = "lm", formula = "y~poly(x,2)")+
   labs(x = "log ratio of coral to algae")+
 # lims(x = c(-1.5,1.5))+
   facet_wrap(~Month)
@@ -140,7 +271,49 @@ anova(CoralMod)
 MacroMod<-lm(daily_GPP~macroalgae*Month, data = LTER1)
 anova(MacroMod)
 
+# GPP vs NEC
+ggplot(LTER1, aes(x = daily_GPP, y = daily_NEC))+
+  geom_point()+
+  geom_smooth(method = "lm")
 
+ggplot(LTER1, aes(x = coral, y = daily_GPP))+
+  geom_point(aes(color = Month))+
+  geom_smooth(method = "lm")
+
+NC<-ggplot(LTER1 %>% drop_na(daily_NEC), aes(x = Year, y = daily_NEC,color = Month))+
+  geom_point(aes(color = Month))+
+  geom_smooth(method = "lm")
+
+ND<-ggplot(LTER1 %>% drop_na(daily_NEC), aes(x = Year, y = night_NEC,color = Month))+
+  geom_point(aes(color = Month))+
+  geom_smooth(method = "lm")
+
+NECMod<-lm(daily_NEC~Year*Month, data = LTER1)
+anova(NECMod)
+
+
+# GPP changes over time regardless of month
+GP<-ggplot(LTER1, aes(x = Year, y = daily_GPP))+
+  geom_point(aes(color = Month))+
+  geom_smooth(method = "lm")
+
+R<-ggplot(LTER1, aes(x = Year, y = daily_R))+
+  geom_point(aes(color = Month))+
+  geom_smooth(method = "lm")
+
+GPPMod<-lm(daily_GPP~Year*Month, data = LTER1)
+anova(GPPMod)
+
+RMod<-lm(daily_R~Year*Month, data = LTER1)
+anova(RMod)
+
+(NC/ND)&theme_bw()|(GP/R)&theme_bw()
+
+#Both GPP and R shrink regardless of month
+
+
+### GPP ~ coral cover, NEC ~ GPP, Show coral cover decline across all 6 sites to show consistency in trend
+## NEC and dissolution are dependent on season
 ## Just coral
 CoralGPP<-LTER1 %>%
   rename(Coral=coral,
