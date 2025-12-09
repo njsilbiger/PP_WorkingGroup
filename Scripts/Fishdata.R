5b6631c61bd4ad31ac4329d0e8b0e7f04b20afd4
Nyssa Silbiger <silbiger@hawaii.edu>
2025-12-08 16:47:46 -1000

    updated fish analysis

--- Scripts/Fishdata.R
+++ Scripts/Fishdata.R
@@ -29,31 +29,35 @@ ggplot(aes(x = Year, y = total_fish_g_m2))+
   theme_bw()
 
 ## group by trophic levels
-fish<-fish %>%
+fish_summary<-fish %>%
   filter(Site == "LTER_1",
          Habitat == "Backreef") %>%
   filter(Biomass < 8000) %>% # there are 3 big sharks in the entire dataset that are biassing the biomass data.  Dropping them
   mutate(Biomass = ifelse(Biomass<0, NA, Biomass)) %>% # negative values are used for missing data
   #mutate(fish_g_m2 = (Biomass/50*Swath)) %>%
   mutate(trophic_new = case_when(Fine_Trophic %in% c("Brusher", "Browser","Excavator","Scraper")~"Benefits from dead coral",
-                                 Fine_Trophic  == "Corallivore"~"Corallivore")
+                                 Fine_Trophic  == "Corallivore"~"Corallivore",
+                                 .default = "Other"
+                                )
   )%>%
- 
-   mutate(fish_kg_m2 = (Biomass/50*Swath)/1000)%>%
+ # mutate(fish_kg_m2 = (Biomass/50*Swath)/1000)%>%
   group_by(Year, trophic_new) %>%
-  summarise(total_biomass = sum(Biomass, na.rm = TRUE),
-            fish_kg_m2_total = sum(fish_kg_m2, na.rm = TRUE)) # 300 m2 per transect x 4 transects per site 
+  summarise(fish_g_m2 = sum(Biomass, na.rm = TRUE)/1200)
+
+#            fish_kg_m2_total = sum(fish_kg_m2, na.rm = TRUE)) # 300 m2 per transect x 4 transects per site 
 
 fish_summary %>%
-  drop_na(trophic_new)%>%
-  ggplot(aes(x = Year, y = fish_kg_m2_total))+
+  #drop_na(trophic_new)%>%
+  ggplot(aes(x = Year, y = fish_g_m2))+
   geom_point()+
   geom_smooth(method = "lm")+
-  add_fishape(#family = "Labridae",
-    option = "Chaetodon_plebeius",
-    xmin = 2010, xmax = 2014, ymin = 0.15, ymax = 0.16,
-    #fill = fish(option = "Chaetodon_plebeius"),
-    alpha = 0.8) +
+  # add_fishape(#family = "Labridae",
+  #   option = "Chaetodon_plebeius",
+  #   xmin = 2010, xmax = 2014, ymin = 0.15, ymax = 0.16,
+  #   #fill = fish(option = "Chaetodon_plebeius"),
+  #   alpha = 0.8) +
   facet_wrap(~trophic_new, scales = "free")
 
 write_csv(fish_clean, here("Data","fish_clean.csv"))
+
+write_csv(fish_summary, here("Data","fish_summary.csv"))
